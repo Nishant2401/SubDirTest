fd40009ab8aa79c642d58cd3d70b29eba19902ae
Nishant Jain <nishant.jain@gtt.com>
2020-05-13 14:25:57 -0500

    Creating new repo

---
+++ LambdaCode/lambda_function.py
@@ -0,0 +1,22 @@
+import json 
+import datetime
+def lambda_handler(event, context):
+    # TODO implement 
+    print(event)
+    data = {
+        'output': 'Hello from '+ event['Country'],
+        #'output': 'Hello from UK',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
+# import boto3
+# import json
+
+# ec2 = boto3.client('ec2')
+# def lambda_handler(event, context):
+#     print(event)
+#     response = ec2.describe_availability_zones()
+#     return {"statusCode": 200, "body": json.dumps(response)}
---
+++ Tests/test_handler.py
@@ -0,0 +1,24 @@
+import unittest
+from LambdaCode import lambda_function as lambdafunc
+
+class TestHandlerCase(unittest.TestCase):
+
+    def test_response(self):
+        print("testing response.")
+        event={'Country':'USA'}
+        #result = lambda_function.lambda_handler(event, None)
+        result = lambdafunc.lambda_handler(event, None)
+        print(result)
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertIn('Hello from '+event['Country'], result['body'])
+        # event={'Country':'INDIA'}
+        # result = lambda_function.lambda_handler(event, None)
+        # print(result)
+        # self.assertEqual(result['statusCode'], 200)
+        # self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        # self.assertIn('Hello from '+event['Country'], result['body'])
+
+
+if __name__ == '__main__':
+    unittest.main()
