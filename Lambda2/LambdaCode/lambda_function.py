9da961f4a98bec039819da0c02ac6a5f43b30d16
Nishant Jain <nishant.jain@gtt.com>
2020-05-18 14:51:53 -0500

    Adding another Lambda function

--- Lambda2/LambdaCode/lambda_function.py
+++ Lambda2/LambdaCode/lambda_function.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
 
-# This is Lambda 1
+# This is Lambda2
 # import boto3
 # import json
 
